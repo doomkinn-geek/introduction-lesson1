b888f844b30944778ae66f6ffccfa96f4f377323
doomkinn-geek <doomkinn@yandex.ru>
2021-04-01 16:38:09 +0300

    Добавьте файлы проекта.

---
+++ task1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var uName = Environment.UserName;
+            Console.WriteLine($"Привет, {uName}, сегодня {System.DateTime.Now:dd.MM.yyyy}");
+        }
+    }
+}
